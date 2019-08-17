85382961d5d4755f55ec984c1a553018bc4ce999
Mario Kostelac <mario.kostelac@gmail.com>
2019-08-17 22:47:55 +0100

    Change DEBUG environment variable to NO_DEAMON
    It represents better what it's doing. It's going to be changed
    with proper CLI in the future.

--- sagemaker_knockout/__main__.py
+++ sagemaker_knockout/__main__.py
@@ -5,10 +5,9 @@ from daemonize import Daemonize
 PIDFILE = os.environ.get('PIDFILE', '/var/run/sagemaker_knockout.pid')
 
 if __name__ == "__main__":
-    debug = os.environ.get('DEBUG', False) == '1'
-    if debug:
+    no_deamon = os.environ.get('NO_DEAMON', False) == '1'
+    if no_deamon:
         sagemaker_knockout.main()
     else:
-        daemon = Daemonize(app='sagemaker_knockout',
-                           pid=PIDFILE, action=sagemaker_knockout.main)
+        daemon = Daemonize(app='sagemaker_knockout', pid=PIDFILE, action=sagemaker_knockout.main)
         daemon.start()
